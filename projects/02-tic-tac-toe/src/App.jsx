eb04aae83e3e1b32a5401f921b788d384a51bb13
Sanvals <santano.sergio@gmail.com>
2024-03-26 13:58:55 +0100

    tictactoe before componentization

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,40 +1,44 @@
 import { useState } from 'react'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const Square = (({ children, updateBoard, index, isSelected})=> {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-  const handleClick = () => updateBoard(index)
-  
-  return (
-    <div 
-      onClick={handleClick}
-      className={className}
-      >
-      {children}
-    </div>
-  )
-})
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinner } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
+    if (board[index] || winner) return
+
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (!newBoard.includes(null)) {
+      setWinner(false)
+    }
+  }
+
+  const clearBoard = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
   }
 
   return (
     <main className='board'>
     <h1>Tic tac toe</h1>
+    <button onClick={clearBoard}>Reset de juego</button> 
     <section className='game'>
       {
         board.map((_, index) => {
@@ -58,6 +62,7 @@ function App() {
         {TURNS.O}
       </Square>
     </section>
+      <WinnerModal clearBoard={clearBoard} winner={winner}/>
     </main>
   )
 }
